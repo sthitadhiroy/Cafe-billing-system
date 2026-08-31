daf16b09ecedcece69e9a6ecae54e989f4830c55
Sthitadhi Roy <user@Users-MacBook-Air.local>
2026-08-31 20:47:45 +0530

    Update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow access from network
+    host: '0.0.0.0',
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
